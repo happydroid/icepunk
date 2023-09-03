8c3c13d0c04455df945b92d54e9fed8842b10965
Artyom Andreyuk <artsiom@threatrix.io>
2023-09-03 22:55:05 +0200

    fix bugs

--- src/main/java/frostpunk/server/ServletInitializer.java
+++ src/main/java/frostpunk/server/ServletInitializer.java
@@ -1,4 +1,4 @@
-package io.threatrix.app;
+package frostpunk.server;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
@@ -7,7 +7,7 @@ public class ServletInitializer extends SpringBootServletInitializer {
 
 	@Override
 	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-		return application.sources(SpringAppApplication.class);
+		return application.sources(ServerApp.class);
 	}
 
 }
